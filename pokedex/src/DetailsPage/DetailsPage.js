e4fef589478cc3a42dd255353d1616c0ff59c760
Zameer Ahmed <thestarshade58@gmail.com>
2023-06-25 22:16:54 +0530

    Tweaked Base Stats and Moves

--- pokedex/src/DetailsPage/DetailsPage.js
+++ pokedex/src/DetailsPage/DetailsPage.js
@@ -14,6 +14,7 @@ function DetailsPage() {
     const { id } = useParams();
     const [pokemonDetails, setPokemonDetails] = useState(null);
     const [pokemonSpecies, setPokemonSpecies] = useState(null);
+    const [moves, setMoves] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState('');
     const [isBookmarked, setIsBookmarked] = useState(false);
@@ -116,13 +117,12 @@ function DetailsPage() {
                                 {pokemonDetails.types.map((types) => <button className='poke-type' style={{ background: typeColor[pokemonType.indexOf(types.type.name)], }} disabled>{types.type.name}</button>)}
                             </div>
                         </div>
-                        <p id='details-id'>{pokemonDetails.id < 10 ? '#00' + pokemonDetails.id : pokemonDetails.id < 100 ? '#0' + pokemonDetails.id : '#' + pokemonDetails.id}</p>
                     </div>
                     <img id='details-img' src={pokemonDetails['sprites']['other']['official-artwork']['front_default']} alt={pokemonDetails.name} />
                 </div>
             </div>
             <div id='details-detail'>
-                <div className='species'>{pokemonSpecies.varieties.map((species) => <button className='poke-variety' onClick={() => handleSpecies(species.pokemon.url)}>{species.pokemon.name.split('-').reverse().join(' ')}</button>)}</div>
+                <div className='species'>{pokemonSpecies.varieties.length > 1 ? pokemonSpecies.varieties.map((species) => <button className='poke-variety' onClick={() => handleSpecies(species.pokemon.url)}>{species.pokemon.name.split('-').reverse().join(' ')}</button>) : null}</div>
                 <ul id='detail-list'>
                     <li onClick={() => handleTabClick('about')} className={activeTab === 'about' ? 'active' : ''} style={activeTab === 'about' ? { color: 'black' } : {}} > About </li>
                     <li onClick={() => handleTabClick('baseStats')} className={activeTab === 'baseStats' ? 'active' : ''} style={activeTab === 'baseStats' ? { color: 'black' } : {}} > Base Stats </li>
@@ -130,22 +130,50 @@ function DetailsPage() {
                     <li onClick={() => handleTabClick('moves')} className={activeTab === 'moves' ? 'active' : ''} style={activeTab === 'moves' ? { color: 'black' } : {}} > Moves </li>
                 </ul>
                 {activeTab === 'about' && <div className='content'>
-                    <p>weight : {pokemonDetails.weight / 10} kg</p>
-                    <p>height : {pokemonDetails.height / 10} m</p>
+                    <p>Id : {pokemonDetails.id < 10 ? '#00' + pokemonDetails.id : pokemonDetails.id < 100 ? '#0' + pokemonDetails.id : '#' + pokemonDetails.id}</p>
+                    <p>Species : {pokemonSpecies.genera.filter(genus => genus.language.name === 'en').map(genus => genus.genus)}</p>
+                    <p>Weight : {pokemonDetails.weight / 10} kg</p>
+                    <p>Height : {pokemonDetails.height / 10} m</p>
                     <p>Abilities : {pokemonDetails.abilities.map((abilities) => (
                         <p key={abilities.ability.name} style={{ marginLeft: '80px' }}>{abilities.ability.name} <span style={{ color: 'gray' }}>{abilities.is_hidden ? '(hidden)' : ''}</span></p>
                     ))}</p>
                 </div>}
-                {activeTab === 'baseStats' && <div className='content'>
-                    {pokemonDetails.stats.map((stats) => (
-                        <p>{stats.stat.name} : {stats.base_stat}</p>
-                    ))}
+                {activeTab === 'baseStats' && <div className='content-stats'>
+                    <div className='stats-name'>
+                        <p>HP</p>
+                        <p>Attack</p>
+                        <p>Defense</p>
+                        <p>Sp. Attack</p>
+                        <p>Sp. Defense</p>
+                        <p>Speed</p>
+                    </div>
+                    <div className='stats'>
+                        {pokemonDetails.stats.map((stats) => (
+                            <p>{stats.base_stat}</p>
+                        ))}
+                    </div>
+                    <div className='bar'>
+                        {pokemonDetails.stats.map((stats) => (
+                            <p style={{ height: '10px', width: stats.base_stat + 'mm', background: stats.base_stat <= 60 ? '#FF7F0F' : stats.base_stat <= 90 ? '#FFDD57' : stats.base_stat <= 120 ? '#A0E515' : stats.base_stat <= 150 ? '#23CD5E' : '#00C2B8' }}></p>
+                        ))}
+                    </div>
                 </div>}
                 {activeTab === 'evolution' && <div className='content'>Not Added yet</div>}
                 {activeTab === 'moves' && <div className='content'>
-                    {pokemonDetails.moves.map((move) => (
-                        <p key={move.move.name}>{move.move.name}</p>
-                    ))}
+                    <h2>Moves</h2>
+                    <div className='moves'>
+                        <div>
+                            <h3>Name</h3>
+                            {pokemonDetails.moves.map((moves) =>
+                                <p>{moves.move.name.replace('-', ' ')}</p>
+                            )}
+                        </div>
+                        <div>
+                            <h3>Learned By</h3>
+                            {pokemonDetails.moves.map((moves) => <p>{moves.version_group_details[0].move_learn_method.name === 'level-up' ? 'lvl ' + moves.version_group_details[0].level_learned_at : moves.version_group_details[0].move_learn_method.name}</p>
+                            )}
+                        </div>
+                    </div>
                 </div>}
             </div>
         </div>
